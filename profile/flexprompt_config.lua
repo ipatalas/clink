e08b8a03cf3d945551fb3b68398414a383a3a15b
Ireneusz Patalas <fido@list.pl>
2022-11-27 20:22:55 +0100

    Update npm prompt

--- profile/flexprompt_config.lua
+++ profile/flexprompt_config.lua
@@ -3,7 +3,7 @@
 -- If you want to make changes, consider copying the file to
 -- 'flexprompt_config.lua' and editing that file instead.
 
-flexprompt.settings.left_prompt = "{histlabel}{cwd}{git:nountracked}{npm}"
+flexprompt.settings.left_prompt = "{histlabel}{cwd}{git:nountracked}{npm:smartname:color=darkyellow}"
 flexprompt.settings.lean_separators = "space"
 flexprompt.settings.symbols.prompt = "λ"
 flexprompt.settings.use_home_tilde = True
